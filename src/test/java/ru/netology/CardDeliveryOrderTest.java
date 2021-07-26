45674071fd65444cebe973da11463d64f4d81d27
Yuliana Smirnova <0387097@gmail.com>
2021-07-26 16:51:13 +0300

    Test update (date corrected)

--- src/test/java/ru/netology/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class CardDeliveryOrderTest {
 
@@ -16,9 +17,10 @@ public class CardDeliveryOrderTest {
     void shouldSentCardOrderWithAllData(){
         Selenide.open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Санкт-Петербург");
-        $("[data-test-id=date] input").click();
-        $("[data-test-id=date] input").setValue("\\b");
-        $("[data-test-id=date] input").setValue(String.valueOf(LocalDate.now().plusDays(3)));
+        $("[data-test-id=date] input").doubleClick();
+        $("[data-test-id=date] input").setValue("\b\b\b\b\b\b\b\b");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        $("[data-test-id=date] input").setValue(formatter.format(LocalDate.now().plusDays(3)));
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+79000000000");
         $("[data-test-id=agreement]").click();
