8ff90bf111d6c20dad6b6baba42e0e44b4b5b461
Yuliana Smirnova <0387097@gmail.com>
2021-07-28 13:27:33 +0300

    Complex elements added to test

--- src/test/java/ru/netology/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/CardDeliveryOrderTest.java
@@ -1,31 +1,38 @@
 package ru.netology;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selectors;
-import com.codeborne.selenide.Selenide;
+
+import com.codeborne.selenide.*;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDate;
+
 import java.time.format.DateTimeFormatter;
 
 public class CardDeliveryOrderTest {
 
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
-
     @Test
-    void shouldSentCardOrderWithAllData() {
+    void shouldSentCardOrderWithAllDataAndComplexElements() {
         Selenide.open("http://localhost:9999/");
-        $("[data-test-id=city] input").setValue("Санкт-Петербург");
+        $("[data-test-id=city] input").setValue("са");
+        $(".input__popup").shouldBe(Condition.visible);
+        $(byText("Санкт-Петербург")).click();
+        $("[data-test-id=city] input").shouldHave(Condition.exactValue("Санкт-Петербург"));
         $("[data-test-id=date] input").doubleClick();
         $("[data-test-id=date] input").setValue("\b\b\b\b\b\b\b\b");
-        $("[data-test-id=date] input").setValue(formatter.format(LocalDate.now().plusDays(3)));
+        $("[data-test-id=date] [type=button]").click();
+        $(".calendar-input__calendar-wrapper").shouldBe(Condition.visible);
+        if((LocalDate.now().plusDays(7)).getMonthValue() != (LocalDate.now()).getMonthValue()) {
+            $("[role=button][data-step='1']").click();
+        }
+        $(byText(String.valueOf(LocalDate.now().plusDays(7).getDayOfMonth()))).click();
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+79000000000");
         $("[data-test-id=agreement]").click();
-        $(Selectors.byText("Забронировать")).click();
-        $(Selectors.byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(byText("Забронировать")).click();
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
     }
 }
